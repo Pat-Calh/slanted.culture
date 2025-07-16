a75d1181bf631697b3465e901b68245874d0f88f
PCalh <pat.calhoun97@gmai.com>
2025-07-15 21:23:31 -0500

    Restore Netlify contact form and remove Google Form embed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Optional: Add a trailing slash to help with static exports
+  trailingSlash: true,
+  // Enable React Strict Mode
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
